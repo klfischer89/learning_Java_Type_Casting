9dd92587c510a4658e16a79da7c6fed988abc1af
klfischer89 <fischer-k4@web.de>
2025-11-27 13:47:50 +0100

    implemented generic list to feed a list of cats or dogs

--- java/App.java
+++ java/App.java
@@ -5,15 +5,22 @@ public class App {
 
     public static void main(String[] args) {
         
-        Cat cat = new Cat();
-        Animal animal = cat;
-        ((Cat) animal).meow();
+        // Cat cat = new Cat();
+        // Animal animal = cat;
+        // ((Cat) animal).meow();
 
         List<Animal> animals = new ArrayList<>();
         animals.add(new Cat());
         animals.add(new Dog());
         new AnimalFeeder().feed(animals);
-        
+
+        AnimalFeederGeneric<Cat> catFeeder = new AnimalFeederGeneric<Cat>(Cat.class);
+        List<Cat> fedAnimals = catFeeder.feed(animals);
+        System.out.println(fedAnimals.get(0));
+
+        AnimalFeederGeneric<Dog> dogFeeder = new AnimalFeederGeneric<Dog>(Dog.class);
+        List<Dog> fedDogs = dogFeeder.feed(animals);
+        System.out.println(fedDogs.get(0));
     }
 
     private static void output(String outStr){
