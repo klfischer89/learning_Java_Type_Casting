a0c0dcb740080b796f15a51ddd7a3985e04f04e8
klfischer89 <fischer-k4@web.de>
2025-11-27 13:22:52 +0100

    added another subclass and class to feed animals

---
+++ java/AnimalFeeder.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class AnimalFeeder {
+
+    public void feed(List<Animal> animals) {
+        animals.forEach(animal -> {
+            animal.eat();
+        });
+    }
+}
